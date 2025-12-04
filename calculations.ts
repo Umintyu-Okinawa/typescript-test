c256ef553536bd7d03246e4f4722a484f4216b14
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-04 17:47:29 +0900

    変更

---
+++ calculations.ts
@@ -0,0 +1,21 @@
+//加算関数（add）：2つの数値を引数として受け取り、その和を返す
+function add(a:number, b:number) :number{
+    return a + b;
+  };
+  
+  add(1,2);
+  
+  console.log(add(1,2));
+
+//減算関数（subtract）：2つの数値を引数として受け取り、その差を返す
+function subtract(a:number, b:number) :number{
+    return a + b;
+  };
+  
+  subtract(1,2);
+  
+  console.log(subtract(1,2));
+
+
+
+  
