fd03547fa228f3cf35eb156ca3248f95dab7ca49
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-08 15:20:45 +0900

    文字列をの"りおんだよ!"から"rion!"に変更しました。

--- Creating a String Manipulation Tool/dist/js/main.js
+++ Creating a String Manipulation Tool/dist/js/main.js
@@ -1,9 +1,9 @@
 import { Length, ToUpperCase, ToLowerCase, Count } from "./string_utils.js";
 //文字列の長さを表示する
-Length("りおんだよ!");
+Length("rion!");
 //文字列を大文字に変換する
-ToUpperCase("りおんだよ!");
+ToUpperCase("rion!");
 //文字列を小文字に変換する
-ToLowerCase("りおんだよ!");
+ToLowerCase("rion!");
 //文字の出現回数を表示する
-Count("りおんだよ!", "l");
+Count("rion", "l");
