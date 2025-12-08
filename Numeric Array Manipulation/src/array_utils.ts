8eb543225b1d48aa990319b857fde6a0db0cc224
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-08 16:12:05 +0900

    課題5のコミットです。ソート出力があっていないため、修正前にコミットしました。

---
+++ Numeric Array Manipulation/src/array_utils.ts
@@ -0,0 +1,37 @@
+//配列の合計値を計算して表示する関数
+export function Sum(array: number[]): void {
+    
+    //配列の合計値を計算して表示する
+    console.log(array.reduce((a, b) => a + b, 0));
+}
+
+//配列の平均値を計算して表示する関数
+export function Average(array: number[]): void {
+
+    //配列の平均値を計算して表示する
+    console.log(array.reduce((a, b) => a + b, 0) / array.length);
+}
+
+
+//配列の最大値を計算して表示する関数
+export function Max(array: number[]): void {
+
+    //配列の最大値を計算して表示する
+    console.log(array.reduce((a, b) => Math.max(a, b), 0));
+}
+
+//配列の最小値を計算して表示する関数
+export function Min(array: number[]): void {
+
+    //配列の最小値を計算して表示する
+    console.log(array.reduce((a, b) => Math.min(a, b), 0));
+
+}
+
+//配列のソートを行う関数
+export function Sort(array: number[]): void {
+
+    //配列のソートを行う
+    console.log(array.sort((a, b) => a - b));
+}
+
